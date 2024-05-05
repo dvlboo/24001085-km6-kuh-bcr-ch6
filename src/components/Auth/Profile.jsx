4fd72af731b7bd4e6ee7e18e06e0cffdb3c1be6e
dvlboo <kuhhhckr@gmail.com>
2024-05-06 00:20:16 +0700

    profile and sidebar

--- src/components/Auth/Profile.jsx
+++ src/components/Auth/Profile.jsx
@@ -1,15 +1,36 @@
-import { Card } from "flowbite-react";
+import { Card, FloatingLabel } from "flowbite-react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProfile } from "../../redux/actions/auth";
+import { useEffect } from "react";
 
 export default function ProfileComponent() {
+
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    // get user profile if we have token
+    const token = localStorage.getItem('token')
+    if (token) {
+      dispatch(getProfile(null,null,null))
+    }
+  }, [dispatch])
+
   return (
     <Card className="flex items-center">
       <h2>My Profile</h2>
-      <Card className="flex items-center w-80 h-80">
-        <img src="/assets/text.png" alt=""/>
+      <Card className="w-80 h-auto">
+        <img  src={user?.photo}/>
       </Card>
-      <p>Name : </p>
-      <p>Email : </p>
-      <p>Role : </p>
+      <div className="mt-4">
+        <FloatingLabel variant="outlined" label="Name" value={user?.name}  color="success" disabled/>
+      </div>
+      <div className="mt-2">
+        <FloatingLabel variant="outlined" label="Email" value={user?.email}  color="success" disabled/>
+      </div>
+      <div className="mt-2">
+        <FloatingLabel variant="outlined" label="Roles" value={user?.roles}  color="success" disabled/>
+      </div>
     </Card>
   )
 }
--- src/components/Navigation/Sidebar.jsx
+++ src/components/Navigation/Sidebar.jsx
@@ -3,18 +3,33 @@ import { Avatar, Sidebar } from "flowbite-react";
 import { HiUser } from "react-icons/hi";
 import { IoCarSport } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { getProfile } from "../../redux/actions/auth";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function SidebarComponent() {
+
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    // get user profile if we have token
+    const token = localStorage.getItem('token')
+    if (token) {
+      dispatch(getProfile(null,null,null))
+    }
+  }, [dispatch])
+
   return (
     <Sidebar aria-label="Users and Cars Menu Dashboard" className="h-screen">
       <Sidebar.Items>
 
         {/* User Info */}
         <Sidebar.ItemGroup>
-          <Avatar size="lg" rounded bordered/>
+          <Avatar size="lg" img={user?.photo} rounded bordered/>
           <div className="text-center space-y-1 font-medium dark:text-white">
-            <div>Name</div>
-            <div className="text-sm text-gray-500 dark:text-gray-400">Email</div>
+            <div>{user?.name} as {user?.roles}</div>
+            <div className="text-sm text-gray-500 dark:text-gray-400">{user?.email}</div>
           </div>
         </Sidebar.ItemGroup>
         
